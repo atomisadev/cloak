65ddc5bd405e029f722ec38bb857da2ee0d34aa0
atomisadev <atomdevelops@pm.me>
2025-12-30 00:45:10 +0000

    feat: implemented secure memory guard

--- pkg/keychain/keychain.go
+++ pkg/keychain/keychain.go
@@ -1,20 +1,48 @@
 package keychain
 
-import "github.com/zalando/go-keyring"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"path/filepath"
+
+	"github.com/zalando/go-keyring"
+)
 
 const (
 	Service = "cloak-cli"
-	User    = "master-key"
 )
 
-func Save(key string) error {
-	return keyring.Set(Service, User, key)
+func GenerateScopeID(path string) (string, error) {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return "", fmt.Errorf("failed to resolve absolute path: %w", err)
+	}
+
+	hash := sha256.Sum256([]byte(absPath))
+	return "project-" + hex.EncodeToString(hash[:8]), nil
 }
 
-func Get() (string, error) {
-	return keyring.Get(Service, User)
+func Save(scopePath, key string) error {
+	user, err := GenerateScopeID(scopePath)
+	if err != nil {
+		return err
+	}
+	return keyring.Set(Service, user, key)
 }
 
-func Delete() error {
-	return keyring.Delete(Service, User)
+func Get(scopePath string) (string, error) {
+	user, err := GenerateScopeID(scopePath)
+	if err != nil {
+		return "", err
+	}
+	return keyring.Get(Service, user)
+}
+
+func Delete(scopePath string) error {
+	user, err := GenerateScopeID(scopePath)
+	if err != nil {
+		return err
+	}
+	return keyring.Delete(Service, user)
 }
